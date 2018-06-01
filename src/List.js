83236ffa66d98e9f1dd0fc8447f6e8df404add2d
Markus <markusmattsson90@gmail.com>
2018-06-01 11:21:39 +0200

    Add file structure and test environment

---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>List</h3>
+      </div>
+    );
+  }
+}
+
+export default List;
+
+/* instructions (swedish): Komponenten ska innehålla ett textfält där användaren
+kan skriva in en text. Där ska finnas en button, som man kan klicka på för att
+lägga till texten i en lista. Komponenten ska visa listan på något lämpligt sätt.
+När listan renderas ska varje element i den förses med en button som kan användas
+för att ta bort elementet. */
---
+++ src/__test__/List.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import List from '../List';
+
+it('renders without crashing', () => {
+  let wrapper = shallow(<List />);
+});
