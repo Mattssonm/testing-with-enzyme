c46211b1a48c45cf68f1b5c9697202e93fe62986
jonasekstrom <dc3nation@hotmail.com>
2018-06-14 22:23:28 +0200

    a few more tests

--- src/__test__/Counter.test.js
+++ src/__test__/Counter.test.js
@@ -9,39 +9,52 @@ describe('Counter test suite', () => {
     ReactDOM.render(<Counter />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('should update the value by 1', () => {
+  it(' makes sure increase button render', () => {
+    let wrapper = shallow(<Counter/>)
+    const increaseBtn = wrapper.find('button').at(0).text();
+    expect(increaseBtn).toEqual('Increase');
+  });
+  it(' makes sure decrease button render', () => {
+    let wrapper = shallow(<Counter/>)
+    const decreaseBtn = wrapper.find('button').at(1).text();
+    expect(decreaseBtn).toEqual('Decrease');
+  });
+  it(' makes sure inputfield render ', () => {
+    let wrapper = shallow(<Counter/>)
+    const counterInput = wrapper.find('input').at(0).length;
+    expect(counterInput).toBe(1)
+  });
+  it(' should update the value by 1', () => {
     const wrapper = shallow(<Counter />);
     const instance = wrapper.instance();
     expect(wrapper.state('value')).toBe(0);
     instance.handleClickIncrease();
     expect(wrapper.state('value')).toBe(1);
   });
-  it('should decrease the value by 1', () => {
+  it(' should decrease the value by 1', () => {
     const wrapper = shallow(<Counter />);
     const instance = wrapper.instance();
     expect(wrapper.state('value')).toBe(0);
     instance.handleClickDecrease();
     expect(wrapper.state('value')).toBe(-1);
   });
-  it('Triggers a new input as the user types', () => {
-    const onSearchStub = jest.fn();
-    const wrapper = shallow(<Counter onChange={onSearchStub}/>);
-    const searchField = wrapper.find('input');
-    const event1 = 5;
-    const event2 = 'dog';
+  it(' works when user inputs number', () => {
+    let wrapper = shallow(<Counter />);
+    const input = wrapper.find('input').at(0)
+    input.simulate('change', { target: { value: 3 } })
+    expect(wrapper.state('value')).toBe(3)
+  })
+  it(' works when user inputs string and converts it to number', () => {
+    let wrapper = shallow(<Counter />);
+    const input = wrapper.find('input').at(0)
+    input.simulate('change', { target: { value: '52' } })
+    expect(wrapper.state('value')).toBe(52)
+  })
+  it(' works when user inputs invalid string', () => {
+    let wrapper = shallow(<Counter />);
+    const inputField = wrapper.find('input').at(0)
+    inputField.simulate('change', { target: { value: 'Glad Sommar' } })
+    expect(wrapper.state('value')).toBe(0)
+  })
 
-    searchField.simulate('change', event1);
-    searchField.simulate('change', event2);
-    assert.equal(onSearchStub.calledTwice, true);
-  });
-  it('textinput with number', () => {
-    const wrapper = shallow(<Counter />);
-    const event = 5;
-    const instance = wrapper.instance();
-    expect(wrapper.state('value')).toBe(0);
-    instance.handleChange(event);
-    expect(wrapper.state('value')).toBe(5);
-  });
 });
-
-// förutom input test så vad mer kan jag testa?
